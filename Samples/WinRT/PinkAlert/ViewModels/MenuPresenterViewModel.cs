f465179a5c5ee4d4f5af41a7f3ca2a66ec66facd
sunnycase <sunnycase@live.cn>
2015-09-21 00:38:22 +0800

    添加midlrt targets

---
+++ Samples/WinRT/PinkAlert/ViewModels/MenuPresenterViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Catel.MVVM;
+
+namespace PinkAlert.ViewModels
+{
+    class MenuPresenterViewModel : ViewModelBase
+    {
+    }
+}
