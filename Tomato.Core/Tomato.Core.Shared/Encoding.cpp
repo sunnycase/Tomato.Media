d0221df3588123e199ebdafdc07f2c7e03def04d
SunnyCase <sunnycase@live.cn>
2016-01-01 15:41:49 +0800

    Add arm target

---
+++ Tomato.Core/Tomato.Core.Shared/Encoding.cpp
@@ -0,0 +1,36 @@
+//
+// Tomato Media
+// 字符编码
+// 
+// (c) SunnyCase 
+// 创建日期 2015-03-18
+#include "pch.h"
+#include "..\..\include\Encoding.h"
+#include "../../include/EncodingDetector.h"
+
+using namespace NS_CORE;
+
+std::wstring NS_CORE::s2ws(const std::string& str, UINT codePage)
+{
+	auto len = MultiByteToWideChar(codePage, 0, str.data(), str.size(), nullptr, 0);
+	std::wstring ws(len, 0);
+	MultiByteToWideChar(codePage, 0, str.data(), str.size(), &ws[0], len);
+
+	return ws;
+}
+
+std::string NS_CORE::ws2s(const std::wstring& str, UINT codePage)
+{
+	auto len = WideCharToMultiByte(codePage, 0, str.data(), str.size(), nullptr, 0, nullptr, nullptr);
+	std::string s(len, 0);
+	WideCharToMultiByte(codePage, 0, str.data(), str.size(), &s[0], len, nullptr, nullptr);
+
+	return s;
+}
+
+std::wstring NS_CORE::s2ws(const std::string& str)
+{
+	static EncodingDetector encDet;
+
+	return s2ws(str, encDet.DetectCodePage(str));
+}
